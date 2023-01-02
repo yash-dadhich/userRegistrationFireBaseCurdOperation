8b90cbbf55832599878d02b48e82cad0a90f97d7
yash <yash.dadhich@squarebits.com>
2023-01-02 16:05:57 +0530

    Select user image and upload to firebase storage

--- app/src/main/java/com/charging/userregistration/AddUserActivity.kt
+++ app/src/main/java/com/charging/userregistration/AddUserActivity.kt
@@ -1,17 +1,39 @@
 package com.charging.userregistration
 
-import androidx.appcompat.app.AppCompatActivity
+import android.Manifest
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.net.Uri
 import android.os.Bundle
+import android.view.View
 import android.widget.Toast
+import androidx.activity.result.ActivityResultCallback
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import com.charging.userregistration.databinding.ActivityAddUserBinding
+import com.google.android.gms.auth.api.signin.internal.Storage
 import com.google.firebase.database.DatabaseReference
 import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.storage.FirebaseStorage
+import com.google.firebase.storage.StorageReference
+import com.squareup.picasso.Picasso
+import java.util.UUID
 
 class AddUserActivity : AppCompatActivity() {
     lateinit var addUserBinding:ActivityAddUserBinding
 
     val database:FirebaseDatabase = FirebaseDatabase.getInstance()
     val myReference:DatabaseReference = database.reference.child("MyUsers")
+
+    lateinit var activityResultLauncher : ActivityResultLauncher<Intent>
+
+    var imageUri : Uri? = null
+
+    val firebaseStorage : FirebaseStorage = FirebaseStorage.getInstance()
+    val storageReference : StorageReference = firebaseStorage.reference
 
 
 
@@ -22,27 +44,113 @@ class AddUserActivity : AppCompatActivity() {
 
         supportActionBar?.title = "Add User"
 
+        // register
+        registerActivityForResult()
+
         addUserBinding.btnAddUser.setOnClickListener {
-            addUserToDatabase()
+            uploadPhoto()
+        }
+
+        addUserBinding.userProfileImage.setOnClickListener {
+            selectUserImage()
         }
     }
 
-    private fun addUserToDatabase(){
-        val name : String = addUserBinding.etName.text.toString()
-        val age : Int = addUserBinding.etAge.text.toString().toInt()
-        val email :String = addUserBinding.etEmail.text.toString()
+    private fun selectUserImage() {
+        if (ContextCompat.checkSelfPermission(
+                this,
+                android.Manifest.permission.READ_EXTERNAL_STORAGE
+            ) != PackageManager.PERMISSION_DENIED
+        ){
+            ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.READ_EXTERNAL_STORAGE),1)
+        }else{
+            val intent = Intent()
+            intent.type = "image/*"
+            intent.action = Intent.ACTION_GET_CONTENT
+            activityResultLauncher.launch(intent)
+        }
 
-        val id :String = myReference.push().key.toString()
+    }
 
-        val user = Users(id,name,age,email)
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+
+        if (requestCode == 1 && grantResults.isNotEmpty() && grantResults[0]== PackageManager.PERMISSION_GRANTED){
+            val intent = Intent()
+            intent.type = "image/*"
+            intent.action = Intent.ACTION_GET_CONTENT
+            activityResultLauncher.launch(intent)
+        }
+
+    }
+
+    fun registerActivityForResult(){
+
+        activityResultLauncher = registerForActivityResult(ActivityResultContracts.StartActivityForResult(),
+            ActivityResultCallback{ result ->
+                val resultCode = result.resultCode
+                val imageData = result.data
+
+                if (resultCode == RESULT_OK && imageData != null){
+                    imageUri = imageData.data
+
+                    imageUri.let {
+                        Picasso.get().load(it).into(addUserBinding.userProfileImage)
+                    }
+                }
+        })
+
+    }
+
+    private fun addUserToDatabase(url:String) {
+        val name: String = addUserBinding.etName.text.toString()
+        val age: Int = addUserBinding.etAge.text.toString().toInt()
+        val email: String = addUserBinding.etEmail.text.toString()
+
+        val id: String = myReference.push().key.toString()
+
+        val user = Users(id, name, age, email,url)
         myReference.child(id).setValue(user).addOnCompleteListener { task ->
             if (task.isSuccessful){
                 Toast.makeText(applicationContext, "User successfully added to database", Toast.LENGTH_SHORT).show()
+
+                addUserBinding.btnAddUser.isClickable = true
+                addUserBinding.progressBar.visibility = View.INVISIBLE
                 finish()
             }
             else{
                 Toast.makeText(applicationContext, task.exception.toString(), Toast.LENGTH_SHORT).show()
             }
         }
+    }
+
+    fun uploadPhoto(){
+        addUserBinding.btnAddUser.isClickable = false
+        addUserBinding.progressBar.visibility = View.VISIBLE
+
+        //UUID
+        val imageName = UUID.randomUUID().toString()
+
+        val imageReference = storageReference.child("images").child(imageName)
+
+        imageUri?.let { uri ->
+            imageReference.putFile(uri).addOnSuccessListener {
+                Toast.makeText(applicationContext, "Image Uploaded successfully", Toast.LENGTH_SHORT).show()
+                val myUploadImageReference = storageReference.child("images").child(imageName)
+                myUploadImageReference.downloadUrl.addOnSuccessListener { url ->
+                    val imageURL = url.toString()
+                    addUserToDatabase(imageURL)
+                }.addOnFailureListener {
+                    Toast.makeText(applicationContext, it.localizedMessage, Toast.LENGTH_SHORT).show()
+                }
+            }.addOnFailureListener {
+                Toast.makeText(applicationContext, it.localizedMessage, Toast.LENGTH_SHORT).show()
+            }
+        }
+
     }
 }
--- app/src/main/java/com/charging/userregistration/Users.kt
+++ app/src/main/java/com/charging/userregistration/Users.kt
@@ -1,8 +1,11 @@
 package com.charging.userregistration
 
+import java.net.URL
+
 data class Users(
     var userId:String = "",
     var userName:String = "",
     var userAge:Int = 0,
-    var userEmail:String = ""
+    var userEmail:String = "",
+    val url: String = ""
 )
